2c7f04c2dc9788f52cf4e8a0535026d0302e1ff6
Saki <sbasken@gmail.com>
2023-04-07 15:44:28 -0700

    Complete lab

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
+
+
 export const SignupForm = () => {
   const [customers, setCustomers] = useState([{}]);
   const [refreshPage, setRefreshPage] = useState(false);
